8dfa0eed9ce4982ffe1f6ca825bb4da0461455b3
946455381@qq.com <wzy19900320>
2020-01-25 10:57:03 +0800

    更新修改妖姬宅爬虫

--- 妖姬宅漫画/index.py
+++ 妖姬宅漫画/index.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import os
+import urllib.request
+from pathlib import Path
 from lxml import etree
 
 class Spider(object):
@@ -9,6 +12,9 @@ class Spider(object):
     url_all = 'http://m.18hm.cc/home/api/cate/tp/1-0-2-1-{}' # 全部
     url_near = 'http://m.18hm.cc/home/api/cate/tp/1-0-1-1-{}' # 已完结的
     url_click = 'http://m.18hm.cc/home/api/cate/tp/1-0-0-1-{}' # 连载中
+    web_url = 'https://www.mkzhan.com'
+    comic_save_path = ''
+    chapter_save_path = ''
 
     def init_spider(self):
         flag = True
@@ -22,7 +28,16 @@ class Spider(object):
                 result = response_data['result']
                 lastPage = result['lastPage']
                 list = result['list']
-                # print(list)
+                print(list)
+                for comic in list:
+                    comic_title = comic['title']
+                    self.comic_save_path = "comic\\" + comic_title.replace(':', '：').replace('?', '？')
+                    path = Path(self.comic_save_path)
+                    if path.exists():
+                        pass
+                    else:
+                        path.mkdir()
+                    self.get_chapter(comic['id'])
                 if lastPage:
                     flag = False
                 else:
@@ -31,10 +46,40 @@ class Spider(object):
             else:
                 flag = False
 
-    def get_chapter(self):
-        url = 'http://m.18hm.cc/home/api/chapter_list/tp/13736-1-1-10'
+    def get_chapter(self, chapter_id):
+        url = 'http://m.18hm.cc/home/api/chapter_list/tp/{}-1-1-200'.format(chapter_id)
         response = requests.get(url, headers=self.headers, timeout=50)
-        print(json.loads(response.text))
+        response_data = json.loads(response.text)
+        result = response_data['result']
+        list = result['list']
+        print(list)
+        # print(len(list))
+        for item in list:
+            print('当前章节：', item['title'])
+            self.chapter_save_path = self.comic_save_path + '\\' + item['title']
+            path = Path(self.chapter_save_path)
+            if path.exists():
+                pass
+            else:
+                path.mkdir()
+            self.down_img(item['imagelist'])
+
+    def down_img(self, image_list):
+        print(image_list.split(','))
+        index = 1
+        image_list = image_list.split(',')
+        for img_url in image_list:
+            image_path = self.chapter_save_path + '\\' + str(index) + '.jpg'
+            if os.path.isfile(image_path):
+                print("此图已经存在:", image_path)
+            else:
+                print("图片正在下载:", image_path)
+                img_url = 'http://17z.online/' + img_url.replace('./', '')
+                print('下载地址：', img_url)
+                urllib.request.urlretrieve(img_url, image_path)
+            index += 1
+
 
 spider = Spider()
-spider.get_chapter()
+spider.init_spider()
+# spider.get_chapter()
