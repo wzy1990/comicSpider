7eac2af5d75378f626409bd56d1d3a5bed54ebeb
江湖最靓的仔 <946455381@qq.com>
2020-12-29 19:13:35 +0800

    更新

--- 51漫画吧/搬运网/搬运网.py
+++ 51漫画吧/搬运网/搬运网.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-# 下面是xpath爬取方法，可跳过vip验证，直接爬取付费内容（漫客栈的vip）
-# @Time    : 2020/1/1 11:20
+# @Time    : 2020/12/28 11:20
 # @Author  : zhiyong_wang
 # @Email   : 946455381@qq.com
-# @File    : 漫客栈.py
+# @File    : 搬运网.py
 # @Software: PyCharm
 
 import requests
@@ -18,6 +17,7 @@ class Spider(object):
         'Cookie': '__cfduid=dfd2b8af05a90dd9077ccf6a9fd2f0cee1608729874; kd5S_2132_saltkey=iX45GG1F; kd5S_2132_lastvisit=1608726274; kd5S_2132_forum_lastvisit=D_103_1608731594D_70_1608731631; kd5S_2132_visitedfid=70D103D46; kd5S_2132_ulastactivity=f4523Bme1ThurQk9ck3n28sumstq127NjzGKkxBzBKjAwZHUMOdI; kd5S_2132_lastcheckfeed=2455716%7C1609163172; kd5S_2132_auth=6539SQSxReTdqYoTJC8rpL6EfogBvXI5De8C2HRUbAssHUNQgzPPq2SpN4aXLTb4Xw%2FfLzBDxDe8ePPB0GdyN0imLDeS; kd5S_2132_tshuz_accountlogin=2455716; kd5S_2132_manhuakaiguan=1; kd5S_2132_manhuamoshi=2; kd5S_2132_manhua_pcadvcookie=10; kd5S_2132_sid=dtb2oO; kd5S_2132_lip=112.97.213.123%2C1609163172; kd5S_2132_lastact=1609164925%09plugin.php%09'
     }
     comic_url = 'http://www.zerobywswit.com/plugin.php?id=jameson_manhua&c=index&a=ku&jindu=1&page={}'
+    comic_url2 = 'http://www.zerobywswit.com/plugin.php?id=jameson_manhua&c=index&a=ku&jindu=1&page={}'
     comic_save_path = ''
     chapter_save_path = ''
 
@@ -30,7 +30,7 @@ class Spider(object):
 
     def save_comic_detail(self, list_url, page_num, save_path):
         post_list = []
-        csv_title = ['标题', '封面', '地址', '是否完结', '内容']
+        csv_title = ['标题', '作者', '封面', '标签', '是否完结', '内容简介', '详情地址']
 
         for index in range(1, page_num):
             print(index)
@@ -48,16 +48,37 @@ class Spider(object):
                 comic_title = comic.find('p', {'class': 'mt5 mb5 uk-text-truncate uk-text-center xs2'}).find('a').text
                 comic_url = comic.find('p', {'class': 'mt5 mb5 uk-text-truncate uk-text-center xs2'}).find('a')['href']
                 comic_isover = '连载中' if '连载' in comic_title else '完结'
-
+                comic_author = ''
                 #print(comic_title, comic_url, comic_isover)
+
                 # 漫画封面
                 img_tag = comic.find('div', {'class': 'uk-card-media-top uk-inline'}).find('img')
                 if img_tag:
                     comic_cover = img_tag['src']
                 else:
                     comic_cover = ''
-                #print('漫画封面:', comic_cover)
-                post_list.append([comic_title, comic_cover, comic_url, comic_isover, ''])
+                
+                tags_list = []
+                try:
+                    comic_url = comic_url.replace('./', 'http://www.zerobywswit.com/')
+                    print(comic_url)
+                    # 获取漫画详情页信息
+                    detail_page = self.get_html(comic_url)
+                    # print(detail_page)
+                    tags_html = []
+                    tags_html.extend(detail_page.find_all('a', {'class': 'uk-label uk-label-border mbn'})) 
+                    tags_html.extend(detail_page.find_all('span', {'class': 'uk-label uk-label-border uk-label-success mbn'}))
+                   
+                    for tags in tags_html:
+                        if '作者' in tags.text:
+                            comic_author = tags.text
+                        else:
+                            tags_list.append(tags.text)
+                    print('标签列表:', ','.join(tags_list))
+                except:
+                    pass
+
+                post_list.append([comic_title, comic_author, comic_cover, ','.join(tags_list), comic_isover, '', comic_url])
 
         post_data = pd.DataFrame(columns=csv_title, data=post_list)
         #print(post_data)
